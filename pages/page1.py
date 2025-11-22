f4c1bbfc256495069980e239c4c27f67a3d8e689
MIKO <moehherry013@gmail.com>
2025-11-22 16:25:21 +0700

    Menambahkan file page1, page2, page3

---
+++ pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("welkam to mikohampek")
+st.write("Ini demo aplikasi streamlit.")
---
+++ pages/page3.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("welkam to mikohampek")
+st.write("This page allows you to customize your preferences.")
+
