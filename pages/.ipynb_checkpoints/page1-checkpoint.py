9c09b3b9a70cffb8e6ab8bdd898261d81c708fae
MIKO <moehherry013@gmail.com>
2025-11-22 17:09:30 +0700

    Remove Jupyter checkpoint files

--- pages/.ipynb_checkpoints/page1-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("welkam to mikohampek")
-st.write("Ini demo aplikasi streamlit.")
--- pages/.ipynb_checkpoints/page3-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("welkam to mikohampek")
-st.write("This page allows you to customize your preferences.")
-
