65ddf0380ef0df5ac1ba28650ee1cbbb050b8f1d
MIKO <moehherry013@gmail.com>
2025-12-12 11:08:56 +0700

    update app py dan hapus page4

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 pages=[
-    st.Page(page="pages/page1.py",title="1",icon="🏚️"),
-    st.Page(page="pages/page2.py",title="2",icon="🫂"),
-    st.Page(page="pages/page3.py",title="3",icon="☂️"),
-    st.Page(page="pages/page4.py",title="4",icon="☂️"),
-]
+    st.Page(page="pages/page1.py",title="Inflasi Dunia",icon="🌍"),
+    st.Page(page="pages/page2.py",title="Pengaturan",icon="🫂"),
+    st.Page(page="pages/page3.py",title="Tentang",icon="☂️"),
+  ]
 
 pg=st.navigation(
     pages,
--- pages/page4.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Welcome to Mico'z Home Page")
-st.write("Ini demo aplikasi streamlit.")
